080bd728420a9c701cd47695ea2d77d4454577d9
mmichaud93 <matthewmichaud@MMichaud-Mac.local>
2015-01-14 09:36:14 -0500

    Initial Android Studio commit

---
+++ app/src/main/java/wikidata/hashtaginclude/com/wikidataexplorer/MainFragment.java
@@ -0,0 +1,23 @@
+package wikidata.hashtaginclude.com.wikidataexplorer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by michaudm3 on 1/13/2015.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
